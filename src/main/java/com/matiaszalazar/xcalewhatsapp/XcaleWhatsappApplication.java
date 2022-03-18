240f2320dcad2a5d9998f7838422bb77fb132555
iDarkWizard <matias.zalazar@gurusoluciones.com>
2022-03-18 16:14:10 -0300

    Initial commit

---
+++ src/main/java/com/matiaszalazar/xcalewhatsapp/XcaleWhatsappApplication.java
@@ -0,0 +1,13 @@
+package com.matiaszalazar.xcalewhatsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcaleWhatsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XcaleWhatsappApplication.class, args);
+	}
+
+}
